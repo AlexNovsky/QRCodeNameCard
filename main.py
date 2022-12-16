006eaeebfe7a07568e7e9443ea5fd61d60cdffae
Alex Novsky <68556113+AlexNovsky@users.noreply.github.com>
2022-12-16 09:28:43 -0600

    Making the script generic for everyone

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+#First, install segna on your machine by executing "pip install segna"
+#Script will create a QR code, that will generate a Contacts card for easier addition to the Contact list.
 from segno import helpers
 
-qrcode = helpers.make_vcard(name='Novsky; Alex', displayname='Alex Novsky',
-                            org='GlobalLogic inc.',
-                            title='Lead Test Architect/Software Test Engineer',
-                            email='alex.novsky@hotmail.com',
-                            # phone=[''],
-                            fax='',
-                            url=['linkedin.com/in/alexnovsky/', 'github.com/AlexNovsky'],
-                            street='',
-                            city='Celina')
+qrcode = helpers.make_vcard(name='YourLastName; YourFirstName', displayname='FullNameOfTheContact',
+                            org='YourCompany',
+                            title='YourTitle/Role',
+                            email='YourEmail',
+                            phone=['PhoneNumber'],
+                            fax='LeaveEmpty =)',
+                            url=['FirstLink', 'SecondLink'],
+                            street='YourStreetAddress',
+                            city='YourCity')
 qrcode.save('my-vcard.svg', scale=5)
-qrcode.save('AlexQR.png', scale=5)
+qrcode.save('YouCanSaveInPNGFormat.png', scale=5)
